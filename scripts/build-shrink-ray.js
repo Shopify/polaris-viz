02ec28d10680644a7c36ea4f8438a3f3e7ef195b
Michelle Chen <michelle.chen@shopify.com>
2020-03-04 16:01:09 -0500

    ⬆️update to shrink-ray 2.0

--- scripts/build-shrink-ray.js
+++ scripts/build-shrink-ray.js
@@ -1,71 +1,96 @@
-/* eslint-disable no-console */
-
-require('isomorphic-fetch');
-
 const {resolve} = require('path');
 const {execSync} = require('child_process');
-const {postReport, sendCommitStatus} = require('@shopify/shrink-ray');
-
-const BASE_BRANCH = 'master';
-const repo = 'polaris-viz';
-const sha = process.env.CIRCLE_SHA1;
-
-const report = resolve(
-  __dirname,
-  '../build/storybook/bundle-analysis/report.html',
-);
+const {readFile} = require('fs-extra');
+const {Logger, Build, ShrinkRayAPI, Check} = require('@shopify/shrink-ray');
 
 process.on('unhandledRejection', (reason) => {
   throw reason;
 });
 
-if (sha) {
-  setupShrinkRay();
-} else {
-  console.log(
-    'sha is not available, building bundle without pinging shrink-ray',
-  );
-  buildPackages();
+startShrinkRayBuild({
+  masterBranchName: 'master',
+  repo: 'polaris-viz',
+  sha: process.env.CIRCLE_SHA1,
+  reportPath: resolve(
+    __dirname,
+    '..',
+    'build/storybook/bundle-analysis/report.html',
+  ),
+  buildUrl: process.env.CIRCLE_BUILD_URL,
+  skip: [Check.Entrypoints],
+});
+
+async function startShrinkRayBuild({
+  masterBranchName,
+  repo,
+  sha,
+  reportPath,
+  buildUrl,
+  skip,
+}) {
+  const logger = new Logger();
+
+  if (sha) {
+    logger.header('Running shrink-ray prebuild script...');
+
+    // fetch latest in pipeline. Travis does a shallow clone by default,
+    // --unshallow makes sure we can fetch the master commit in case it is not
+    // included in the initial shallow clone
+    execSync('git fetch --unshallow origin master:refs/remotes/origin/master');
+
+    const masterSha = execSync(
+      `git merge-base HEAD origin/${masterBranchName}`,
+      {
+        encoding: 'utf8',
+      },
+    ).trim();
+
+    const shrinkRay = new ShrinkRayAPI();
+    const build = new Build({
+      repo,
+      sha,
+      masterSha,
+    });
+
+    try {
+      logger.header('Validating...');
+      build.validate();
+      logger.for('validation').log('✅ sha validation successful!');
+
+      logger.header('Initializing check(s) on GitHub');
+      console.log('buildUrl', buildUrl);
+      await shrinkRay.create({...build, buildUrl, skip});
+
+      buildStorybookBuild();
+
+      logger.header(`Uploading build report to Shrink-Ray`);
+      await shrinkRay.report(build, await bundleAnalyzerReport(reportPath));
+    } catch (error) {
+      logger.header('shrink-ray build fail');
+      logger.for('error').log(error);
+      logger.for('error').log(`sending status to server for clean up `);
+      await shrinkRay.error(build, error);
+    }
+  } else {
+    buildStorybookBuild();
+  }
 }
 
-function buildPackages() {
-  execSync('yarn run storybook:build', {
+function buildStorybookBuild() {
+  const logger = new Logger();
+
+  logger.header('Running storybook build...');
+  execSync('yarn run storybook:build --quiet', {
     stdio: 'inherit',
   });
 }
 
-function setupShrinkRay() {
-  console.log('[shrink-ray] Running shrink-ray prebuild script...');
-
-  // fetch latest in BuildKite pipeline
-  execSync('git fetch origin master');
-
-  const masterSha = execSync(`git merge-base HEAD origin/${BASE_BRANCH}`, {
-    encoding: 'utf8',
-  }).trim();
-
-  sendCommitStatus({repo, sha, state: 'pending'})
-    .then(() => {
-      console.log('[shrink-ray] shrink-ray prebuild script completed.');
-      buildPackages();
-
-      return postReport({
-        repo,
-        sha,
-        baselineSha: masterSha,
-        reportPath: report,
-      });
-    })
-    .catch((error) => {
-      console.log('ERROR: ', error);
-
-      // eslint-disable-next-line promise/no-nesting
-      sendCommitStatus({repo, sha, state: 'error'})
-        .then(() => {
-          throw error;
-        })
-        .catch(() => {
-          throw error;
-        });
-    });
+function bundleAnalyzerReport(reportPath) {
+  try {
+    return readFile(reportPath, 'utf-8');
+  } catch (error) {
+    throw new Error(
+      `Could not read webpack-bundle-analyzer report: ${reportPath}`,
+    );
+  }
 }
