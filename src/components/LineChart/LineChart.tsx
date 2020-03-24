dc10e59c0768708d71d56ea5155da774d8d4eca4
Andy Mockler <asmockler@gmail.com>
2020-03-23 23:32:01 -0400

    Basic line chart
    X Axis Grid Lines
    Allow for dashed lines
    Line colors
    Basic Y Axis

---
+++ src/components/LineChart/LineChart.tsx
@@ -0,0 +1,105 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {line} from 'd3-shape';
+import {scaleLinear} from 'd3-scale';
+
+import {Margin, Y_SCALE_PADDING} from './constants';
+import {ChartDimensions, Series} from './types';
+import {yAxisMinMax} from './utilities';
+import {Line, XAxis, YAxis} from './components';
+
+interface Props {
+  series: Series[];
+  xAxisLabels: string[];
+  formatYAxisValue?(value: number): string;
+}
+
+export function LineChart({
+  series,
+  xAxisLabels,
+  formatYAxisValue = (value) => `${value}`,
+}: Props) {
+  const [svgDimensions, setSvgDimensions] = useState<ChartDimensions>({
+    width: 0,
+    height: 0,
+  });
+  const containerRef = useRef<SVGSVGElement | null>(null);
+
+  function updateDimensions() {
+    if (containerRef.current != null) {
+      setSvgDimensions({
+        height: containerRef.current.clientHeight,
+        width: containerRef.current.clientWidth,
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (containerRef.current != null) {
+      updateDimensions();
+    }
+
+    window.addEventListener('resize', updateDimensions);
+
+    return () => {
+      window.removeEventListener('resize', updateDimensions);
+    };
+  }, [containerRef]);
+
+  const longestSeriesLength = series.reduce<number>(
+    (max, currentSeries) => Math.max(max, currentSeries.data.length - 1),
+    0,
+  );
+
+  const [minY, maxY] = yAxisMinMax(series);
+
+  const xScale = scaleLinear()
+    .range([0, svgDimensions.width - Margin.Left - Margin.Right])
+    .domain([0, longestSeriesLength]);
+
+  const yScale = scaleLinear()
+    .range([svgDimensions.height - (Margin.Top + Margin.Bottom), 0])
+    .domain([Math.min(0, minY), maxY * Y_SCALE_PADDING])
+    .nice();
+
+  const lineGenerator = line<{x: string; y: number}>()
+    .x((_, index) => xScale(index))
+    .y(({y}) => yScale(y));
+
+  return (
+    <svg width="100%" height="100%" ref={containerRef}>
+      <g transform={`translate(0,${svgDimensions.height - Margin.Bottom})`}>
+        <XAxis
+          xScale={xScale}
+          labels={xAxisLabels}
+          dimensions={svgDimensions}
+        />
+      </g>
+
+      <g transform={`translate(${Margin.Left},${Margin.Top})`}>
+        <YAxis
+          yScale={yScale}
+          formatYAxisValue={formatYAxisValue}
+          dimensions={svgDimensions}
+        />
+      </g>
+
+      <g transform={`translate(${Margin.Left},${Margin.Top})`}>
+        {series
+          .slice()
+          .reverse()
+          .map((singleSeries) => {
+            const {data, name} = singleSeries;
+            const path = lineGenerator(data);
+
+            if (path == null) {
+              throw new Error(
+                `Could not generate line path for series ${name}`,
+              );
+            }
+
+            return <Line key={name} series={singleSeries} path={path} />;
+          })}
+      </g>
+    </svg>
+  );
+}
---
+++ src/components/LineChart/components/YAxis/YAxis.tsx
@@ -0,0 +1,59 @@
+import React, {useMemo} from 'react';
+import {ScaleLinear} from 'd3-scale';
+import {
+  colorSkyDark,
+  colorInkLighter,
+  colorInkLightest,
+  spacingTight,
+} from '@shopify/polaris-tokens';
+
+import {Margin} from '../../constants';
+import {ChartDimensions} from '../../types';
+
+interface Props {
+  yScale: ScaleLinear<number, number>;
+  formatYAxisValue(value: number): string;
+  dimensions: ChartDimensions;
+}
+
+const MIN_LABEL_SPACE = 80;
+
+export function YAxis({yScale, formatYAxisValue, dimensions}: Props) {
+  const ticks = useMemo(() => {
+    const maxTicks = Math.max(
+      1,
+      Math.floor(dimensions.height / MIN_LABEL_SPACE),
+    );
+
+    return yScale.ticks(maxTicks).map((value) => ({
+      value,
+      formattedValue: formatYAxisValue(value),
+      yOffset: yScale(value),
+    }));
+  }, [dimensions.height, formatYAxisValue, yScale]);
+
+  return (
+    <g>
+      {ticks.map(({value, formattedValue, yOffset}) => {
+        return (
+          <g key={value} transform={`translate(0,${yOffset})`}>
+            <line
+              x2={`${dimensions.width - Margin.Left - Margin.Right}`}
+              stroke={value === 0 ? colorInkLightest : colorSkyDark}
+            />
+            <text
+              fill={colorInkLighter}
+              style={{
+                fontSize: '12px',
+                textAnchor: 'end',
+                transform: `translateX(-${spacingTight})`,
+              }}
+            >
+              {formattedValue}
+            </text>
+          </g>
+        );
+      })}
+    </g>
+  );
+}
