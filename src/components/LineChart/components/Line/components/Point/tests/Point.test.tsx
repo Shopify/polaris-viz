b5c09f8e6d07907c91875c6cfe16b424436765e5
Andy Mockler <asmockler@gmail.com>
2020-05-20 11:32:46 -0700

    :test_tube: Add tests for <Line /> and <Point />

---
+++ src/components/LineChart/components/Line/components/Point/tests/Point.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {mount} from '@shopify/react-testing';
+import {Color} from 'types';
+
+import {Point} from '../Point';
+
+const mockProps = {
+  cx: 100,
+  cy: 100,
+  active: false,
+  color: 'colorPurple' as Color,
+};
+
+describe('<Point />', () => {
+  it('renders a circle at the given coordinates', () => {
+    const point = mount(
+      <svg>
+        <Point {...mockProps} />
+      </svg>,
+    );
+
+    expect(point).toContainReactComponent('circle', {
+      cx: 100,
+      cy: 100,
+    });
+  });
+
+  it('renders with a radius of 5 when active', () => {
+    const point = mount(
+      <svg>
+        <Point {...mockProps} active />
+      </svg>,
+    );
+
+    expect(point).toContainReactComponent('circle', {
+      // eslint-disable-next-line id-length
+      r: 5,
+    });
+  });
+
+  it('renders with a radius of 5 when not active', () => {
+    const point = mount(
+      <svg>
+        <Point {...mockProps} active={false} />
+      </svg>,
+    );
+
+    expect(point).toContainReactComponent('circle', {
+      // eslint-disable-next-line id-length
+      r: 0,
+    });
+  });
+});
