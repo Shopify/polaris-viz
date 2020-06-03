68ccf14895f7278edda1e342cabbdb431b20b871
Andy Mockler <asmockler@gmail.com>
2020-06-03 14:15:51 -0700

    Make y-axis margin dynamic

--- src/components/LineChart/components/YAxis/tests/YAxis.test.tsx
+++ src/components/LineChart/components/YAxis/tests/YAxis.test.tsx
@@ -1,53 +1,19 @@
 import React from 'react';
 import {mount} from '@shopify/react-testing';
-import {scaleLinear} from 'd3-scale';
 
 import {YAxis} from '../YAxis';
 
-jest.mock('d3-scale', () => ({
-  scaleLinear: () => {
-    function scale(value: any) {
-      return value;
-    }
-
-    scale.ticks = () => [0, 1, 2];
-    scale.range = () => [0, 2];
-
-    return scale;
-  },
-}));
-
-(global as any).DOMRect = class DOMRect {
-  width = 500;
-  height = 160;
-};
+const testTicks = [
+  {value: 0, formattedValue: '0', yOffset: 0},
+  {value: 1000, formattedValue: '1,000', yOffset: 50},
+  {value: 2000, formattedValue: '2,000', yOffset: 100},
+];
 
 describe('<YAxis />', () => {
-  it("provides a best estimate number of ticks to d3's ticks utility to choose the ideal number of ticks", () => {
-    const yScale = scaleLinear();
-    const ticksSpy = jest.spyOn(yScale, 'ticks');
-
-    mount(
-      <svg>
-        <YAxis
-          yScale={yScale}
-          formatYAxisValue={(value) => value.toString()}
-          dimensions={new DOMRect()}
-        />
-      </svg>,
-    );
-
-    expect(ticksSpy).toHaveBeenCalledWith(2);
-  });
-
   it('draws a horizontal grid line for each tick', () => {
     const yAxis = mount(
       <svg>
-        <YAxis
-          yScale={scaleLinear()}
-          formatYAxisValue={(value) => value.toString()}
-          dimensions={new DOMRect()}
-        />
+        <YAxis ticks={testTicks} drawableWidth={500} />
       </svg>,
     );
 
@@ -57,19 +23,14 @@ describe('<YAxis />', () => {
     // so we have to do these assertions manually
     expect(lines).toHaveLength(3);
     lines.forEach((line) => {
-      // 420 is the mocked width of 500 from above minus the left and right margins
-      expect(line.prop('x2')).toStrictEqual('420');
+      expect(line.prop('x2')).toStrictEqual(500);
     });
   });
 
   it('displays a formatted value with each tick', () => {
     const yAxis = mount(
       <svg>
-        <YAxis
-          yScale={scaleLinear()}
-          formatYAxisValue={(value) => `formatted: ${value}`}
-          dimensions={new DOMRect()}
-        />
+        <YAxis ticks={testTicks} drawableWidth={500} />
       </svg>,
     );
 
@@ -77,10 +38,6 @@ describe('<YAxis />', () => {
     // so we have to do these assertions manually
     const text = yAxis.findAll('text')!;
     const values = text.map((node) => node.prop('children'));
-    expect(values).toStrictEqual([
-      'formatted: 0',
-      'formatted: 1',
-      'formatted: 2',
-    ]);
+    expect(values).toStrictEqual(['0', '1,000', '2,000']);
   });
 });
