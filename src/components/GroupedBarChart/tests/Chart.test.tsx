aa976876158bea2af3e0701899dfa01bf9021919
Kyle Kilbride <kyle.kilbride@shopify.com>
2020-10-22 14:36:01 -0400

    update test name

--- src/components/GroupedBarChart/tests/Chart.test.tsx
+++ src/components/GroupedBarChart/tests/Chart.test.tsx
@@ -105,7 +105,7 @@ describe('Chart />', () => {
     });
   });
 
-  it('passes highlightColors barGroup', () => {
+  it('passes highlightColors to barGroup', () => {
     const chart = mount(
       <Chart
         {...mockProps}
