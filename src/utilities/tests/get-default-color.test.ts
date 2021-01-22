a1ce4e127c211d335dc1f8e33e2930b3d79d8ef5
Trevor Harmon <trevor.harmon@shopify.com>
2021-01-22 14:20:41 -0500

    more pr fixes

--- src/utilities/tests/get-default-color.test.ts
+++ src/utilities/tests/get-default-color.test.ts
@@ -12,9 +12,16 @@ describe('getDefaultColor()', () => {
     expect(getDefaultColor(3)).toStrictEqual('quaternary');
   });
 
-  it('throws an error if the index provided is greater than the number of default colors', () => {
-    expect(() => getDefaultColor(4)).toThrow(
+  it('warns if the index provided is greater than the number of default colors', () => {
+    const consoleSpy = jest.spyOn(console, 'warn');
+    const environment = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'development';
+
+    expect(getDefaultColor(4)).toStrictEqual('primary');
+    expect(consoleSpy).toHaveBeenCalledWith(
       'There are too many series to rely on default color values. Please pass a color value for every series',
     );
+
+    process.env.NODE_ENV = environment;
   });
 });
