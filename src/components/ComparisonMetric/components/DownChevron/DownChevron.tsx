78d488bb05730b7f98a4ea793a5a2a0894c1ed77
Shameel Abdullah <shameel.abdullah@shopify.com>
2021-02-25 21:29:46 -0500

    Use visuallyhidden with aria labels

--- src/components/ComparisonMetric/components/DownChevron/DownChevron.tsx
+++ src/components/ComparisonMetric/components/DownChevron/DownChevron.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {getColorValue} from '../../../../utilities';
 
+import styles from './DownChevron.scss';
+
 interface Props {
   accessibilityLabel: string;
 }
@@ -9,18 +11,21 @@ interface Props {
 export function DownChevron({accessibilityLabel}: Props) {
   const fill = getColorValue('negative');
   return (
-    <svg
-      aria-label={accessibilityLabel}
-      style={{fill}}
-      width="8"
-      height="10"
-      viewBox="0 0 8 10"
-    >
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M4.4241 9.6244L7.4241 6.6244C7.6587 6.3898 7.6587 6.0106 7.4241 5.776C7.1895 5.5414 6.8103 5.5414 6.5757 5.776L4.5999 7.7518V0.800195C4.5999 0.468395 4.3317 0.200195 3.9999 0.200195C3.6681 0.200195 3.3999 0.468395 3.3999 0.800195V7.7518L1.4241 5.776C1.1895 5.5414 0.810302 5.5414 0.575702 5.776C0.458702 5.893 0.399902 6.0466 0.399902 6.2002C0.399902 6.3538 0.458702 6.5074 0.575702 6.6244L3.5757 9.6244C3.8103 9.859 4.1895 9.859 4.4241 9.6244Z"
-      />
-    </svg>
+    <React.Fragment>
+      <span className={styles.VisuallyHidden}>{accessibilityLabel}</span>
+      <svg
+        aria-hidden="true"
+        style={{fill}}
+        width="8"
+        height="10"
+        viewBox="0 0 8 10"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M4.4241 9.6244L7.4241 6.6244C7.6587 6.3898 7.6587 6.0106 7.4241 5.776C7.1895 5.5414 6.8103 5.5414 6.5757 5.776L4.5999 7.7518V0.800195C4.5999 0.468395 4.3317 0.200195 3.9999 0.200195C3.6681 0.200195 3.3999 0.468395 3.3999 0.800195V7.7518L1.4241 5.776C1.1895 5.5414 0.810302 5.5414 0.575702 5.776C0.458702 5.893 0.399902 6.0466 0.399902 6.2002C0.399902 6.3538 0.458702 6.5074 0.575702 6.6244L3.5757 9.6244C3.8103 9.859 4.1895 9.859 4.4241 9.6244Z"
+        />
+      </svg>
+    </React.Fragment>
   );
 }
