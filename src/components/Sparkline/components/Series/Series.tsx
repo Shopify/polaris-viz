d7dff529959bc04ff47290b4b6cea956398c0e24
Carys Mills <carys.mills@shopify.com>
2021-03-09 11:05:38 -0500

    Fix circular dependency in Sparkline

--- src/components/Sparkline/components/Series/Series.tsx
+++ src/components/Sparkline/components/Series/Series.tsx
@@ -5,7 +5,7 @@ import {area} from 'd3-shape';
 import {getColorValue, uniqueId, rgbToRgba} from '../../../../utilities';
 import {usePrefersReducedMotion} from '../../../../hooks';
 import {SingleSeries, Coordinates} from '../../Sparkline';
-import {LinearGradient} from '../../components';
+import {LinearGradient} from '../LinearGradient';
 
 import styles from './Series.scss';
 
