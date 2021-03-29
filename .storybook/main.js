dc417a895866f44ac04981915f730416ef6d7261
Petar Bojinov <petar.bojinov@shopify.com>
2021-03-29 12:17:27 -0700

    Add Storybook, Chromatic, and BarChart story

---
+++ .storybook/main.js
@@ -0,0 +1,73 @@
+// Inspired by shopify/polaris-react storybook config
+// https://github.com/Shopify/polaris-react/blob/main/.storybook/main.js
+
+const path = require('path');
+
+const postcssShopify = require('@shopify/postcss-plugin');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    {name: '@storybook/addon-essentials', options: {docs: true}},
+  ],
+  typescript: {
+    // also valid 'react-docgen-typescript' | false
+    reactDocgen: 'react-docgen-typescript',
+  },
+  webpackFinal: (config) => {
+    const isProduction = config.mode === 'production';
+
+    // Shrink ray only strips hashes when comparing filenames with this format.
+    // Without this there will be lots of "add 1 file and removed 1 file" notices.
+    config.output.filename = '[name]-[hash].js';
+
+    const extraRules = [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: false,
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]-[local]_[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => postcssShopify(),
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+    ];
+
+    config.module.rules = [
+      // Strip out existing rules that apply to md files
+      ...config.module.rules.filter(
+        (rule) => rule.test.toString() !== '/\\.md$/',
+      ),
+      ...extraRules,
+    ];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@shopify/polaris-viz': path.resolve(__dirname, '..', 'src'),
+    };
+    return config;
+  },
+};
---
+++ src/components/BarChart/stories/BarChart.stories.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Story, Meta} from '@storybook/react';
+
+import {BarChart, BarChartProps} from '../../../components';
+import {Color} from '../../../types';
+
+import {
+  colorOptions,
+  getDataPoint,
+  formatXAxisLabel,
+  formatYAxisLabel,
+  renderTooltipContent,
+} from './utils.stories';
+import styles from './BarChart.stories.scss';
+
+const primaryColor = colorOptions[0] as Color;
+const secondaryColor = colorOptions[1] as Color;
+
+export default {
+  title: 'BarChart',
+  component: BarChart,
+  decorators: [
+    (Story: any) => <div className={styles.Container}>{Story()}</div>,
+  ],
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: colorOptions,
+        defaultValue: primaryColor,
+      },
+    },
+    highlightColor: {
+      control: {
+        type: 'select',
+        options: colorOptions,
+        defaultValue: secondaryColor,
+      },
+    },
+    // Render the prop documentation but without a control.
+    formatXAxisLabel: {
+      control: false,
+    },
+    formatYAxisLabel: {
+      control: false,
+    },
+    renderTooltipContent: {
+      control: false,
+    },
+  },
+} as Meta;
+
+const Template: Story<BarChartProps> = (args: BarChartProps) => {
+  return <BarChart {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  data: [
+    {rawValue: getDataPoint(), label: '2020-01-01T12:00:00Z'},
+    {rawValue: getDataPoint(), label: '2020-01-02T12:00:00Z'},
+    {rawValue: getDataPoint(), label: '2020-01-03T12:00:00Z'},
+    {rawValue: getDataPoint(), label: '2020-01-04T12:00:00Z'},
+    {rawValue: getDataPoint(), label: '2020-01-05T12:00:00Z'},
+    {rawValue: getDataPoint(), label: '2020-01-06T12:00:00Z'},
+  ],
+  color: primaryColor,
+  highlightColor: secondaryColor,
+  formatXAxisLabel,
+  formatYAxisLabel,
+  renderTooltipContent,
+};
