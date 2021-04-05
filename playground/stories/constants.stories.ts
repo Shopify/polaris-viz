b6930a15404b0cc7b3c27c9ae49e4e8a8e71d452
Carys Mills <carys.mills@shopify.com>
2021-04-05 09:38:28 -0400

    add constants file to fix build

---
+++ playground/stories/constants.stories.ts
@@ -0,0 +1,31 @@
+export const backgroundColors = {
+  light: '#fff',
+  dark: '#1f1f25',
+};
+
+export const chartColors = {blueGreenGradient: 'blueGreenGradient'};
+
+export const axisColors = {
+  dark: 'rgb(65, 66, 71)',
+  light: 'rgb(223, 227, 232)',
+};
+
+export const textColors = {
+  light: 'rgb(99, 115, 129)',
+  dark: 'rgb(220, 220, 220)',
+};
+
+export const crossHairColors = {
+  light: '',
+  dark: '',
+};
+
+export const fontFamily =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+
+export const containerStyles = {
+  width: '600px',
+  height: '250px',
+  padding: '20px',
+  marginTop: '5px',
+};
