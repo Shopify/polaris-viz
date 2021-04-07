b568dee54c23e5b8baeb707fa315c53249aa48e7
Carys Mills <carys.mills@shopify.com>
2021-04-07 12:08:29 -0400

    improve legend

--- src/components/LinePreview/LinePreview.tsx
+++ src/components/LinePreview/LinePreview.tsx
@@ -12,18 +12,18 @@ interface Props {
 export function LinePreview({color, lineStyle}: Props) {
   return (
     <div>
-      <svg width="15px" height="5px">
+      <svg width="23px" height="5px">
         <path
           d={`M1, ${lineStyle === 'dashed' ? 2 : 0} L100,0`}
           fill="none"
           strokeLinejoin="round"
           stroke={getColorValue(color)}
-          strokeWidth={lineStyle === 'dashed' ? `${1.5}px` : `${4}px`}
+          strokeWidth={lineStyle === 'dashed' ? `${3}px` : `${4}px`}
           {...(lineStyle === 'dashed'
             ? {
                 ...{
                   strokeLinecap: 'round',
-                  strokeDasharray: '0.2, 3',
+                  strokeDasharray: '0.1, 8',
                 },
               }
             : undefined)}
