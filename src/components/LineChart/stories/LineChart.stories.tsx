48e5d9dae2381f294ad5dc74a321ba6bc01f3e90
Philipp Schöfer <philipp.schofer@shopify.com>
2021-04-16 11:26:16 -0400

    Creates Storybooks story for `LineChart`

---
+++ src/components/LineChart/stories/LineChart.stories.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import {Story, Meta} from '@storybook/react';
+
+import {LineChart, LineChartProps} from '../../../components';
+import {Color} from '../../../types';
+
+import {
+  colorOptions,
+  getDataPoint,
+  formatXAxisLabel,
+  formatYAxisLabel,
+  renderTooltipContent,
+} from './utils.stories';
+import styles from './LineChart.stories.scss';
+
+const primaryColor = colorOptions[0] as Color;
+const secondaryColor = colorOptions[1] as Color;
+
+const series = [
+  {
+    name: 'Apr 01–Apr 14, 2020',
+    data: [
+      {rawValue: 2251, label: '2020-04-01T12:00:00'},
+      {rawValue: 12132.2, label: '2020-04-02T12:00:00'},
+      {rawValue: 5000, label: '2020-04-03T12:00:00'},
+      {rawValue: 7200, label: '2020-04-04T12:00:00'},
+      {rawValue: 1500, label: '2020-04-05T12:00:00'},
+      {rawValue: 6132, label: '2020-04-06T12:00:00'},
+      {rawValue: 3100, label: '2020-04-07T12:00:00'},
+      {rawValue: 2200, label: '2020-04-08T12:00:00'},
+      {rawValue: 5103, label: '2020-04-09T12:00:00'},
+      {rawValue: 2112.5, label: '2020-04-10T12:00:00'},
+      {rawValue: 4004, label: '2020-04-11T12:00:00'},
+      {rawValue: 6000, label: '2020-04-12T12:00:00'},
+      {rawValue: 5500, label: '2020-04-13T12:00:00'},
+      {rawValue: 7000, label: '2020-04-14T12:00:00'},
+    ],
+    color: 'primary',
+  },
+  {
+    name: 'Mar 01–Mar 14, 2020',
+    data: [
+      {rawValue: 5200, label: '2020-03-01T12:00:00'},
+      {rawValue: 7000, label: '2020-03-02T12:00:00'},
+      {rawValue: 1000, label: '2020-03-03T12:00:00'},
+      {rawValue: 2000, label: '2020-03-04T12:00:00'},
+      {rawValue: 5000, label: '2020-03-05T12:00:00'},
+      {rawValue: 1000, label: '2020-03-06T12:00:00'},
+      {rawValue: 2000, label: '2020-03-07T12:00:00'},
+      {rawValue: 5000, label: '2020-03-08T12:00:00'},
+      {rawValue: 4000, label: '2020-03-09T12:00:00'},
+      {rawValue: 11200, label: '2020-03-10T12:00:00'},
+      {rawValue: 2000, label: '2020-03-11T12:00:00'},
+      {rawValue: 3000, label: '2020-03-12T12:00:00'},
+      {rawValue: 2000, label: '2020-03-13T12:00:00'},
+      {rawValue: 3000, label: '2020-03-14T12:00:00'},
+    ],
+    color: 'pastComparison',
+    lineStyle: 'dashed' as 'dashed',
+  },
+];
+
+const xAxisLabels = series[0].data.map(({label}) => label);
+
+export default {
+  title: 'LineChart',
+  component: LineChart,
+  decorators: [
+    (Story: any) => <div className={styles.Container}>{Story()}</div>,
+  ],
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: colorOptions,
+        defaultValue: primaryColor,
+      },
+    },
+    highlightColor: {
+      control: {
+        type: 'select',
+        options: colorOptions,
+        defaultValue: secondaryColor,
+      },
+    },
+    // Render the prop documentation but without a control.
+    formatXAxisLabel: {
+      control: false,
+    },
+    formatYAxisLabel: {
+      control: false,
+    },
+    renderTooltipContent: {
+      control: false,
+    },
+  },
+} as Meta;
+
+const Template: Story<LineChartProps> = (args: LineChartProps) => {
+  return <LineChart {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  series,
+  xAxisLabels,
+  formatXAxisLabel,
+  formatYAxisLabel,
+  renderTooltipContent,
+  skipLinkText: 'Skip chart content',
+};
---
+++ src/components/LineChart/stories/utils.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import tokens from '@shopify/polaris-tokens';
+
+import {BarChartTooltipContent} from '../../../components';
+import {vizColors} from '../../../utilities';
+
+const polarisTokensColors = Object.keys(tokens).filter((key) =>
+  key.startsWith('color'),
+);
+
+export const colorOptions: string[] = Object.keys(vizColors).concat(
+  polarisTokensColors,
+);
+
+export const getDataPoint = (limit = 1000) => {
+  return Math.random() * limit;
+};
+
+export function formatXAxisLabel(value: string) {
+  return new Date(value).toLocaleDateString('en-CA', {
+    day: 'numeric',
+    month: 'short',
+  });
+}
+
+export function formatYAxisLabel(value: number) {
+  return new Intl.NumberFormat('en-CA', {
+    style: 'currency',
+    currency: 'CAD',
+    maximumSignificantDigits: 2,
+  }).format(value);
+}
+
+export function renderTooltipContent({
+  label,
+  value,
+}: {
+  label: string;
+  value: number;
+}) {
+  function formatTooltipLabel(value: string) {
+    debugger;
+    return new Date(value).toLocaleDateString('en-CA', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+  }
+
+  function formatTooltipValue(value: number) {
+    return new Intl.NumberFormat('en-CA', {
+      style: 'currency',
+      currency: 'CAD',
+    }).format(value);
+  }
+
+  const formattedLabel = formatTooltipLabel(label);
+  const formattedValue = formatTooltipValue(value);
+
+  return (
+    <BarChartTooltipContent label={formattedLabel} value={formattedValue} />
+  );
+}
