301023042797dd145bf4c8d325a9ffaa2b02c423
Carys Mills <carys.mills@shopify.com>
2021-04-30 17:42:53 -0400

    try CSS tooltipcontainer

--- src/components/TooltipContainer/TooltipContainer.tsx
+++ src/components/TooltipContainer/TooltipContainer.tsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useRef, useState, ReactNode} from 'react';
-import {useSpring, animated} from 'react-spring';
+import React, {useRef, ReactNode} from 'react';
 
 import {clamp} from '../../utilities';
 
@@ -20,7 +19,6 @@ interface Props {
 const TOOLTIP_MARGIN = 10;
 
 export function TooltipContainer({
-  activePointIndex,
   currentX,
   currentY,
   chartDimensions,
@@ -30,103 +28,76 @@ export function TooltipContainer({
   id = '',
 }: Props) {
   const tooltipRef = useRef<HTMLDivElement | null>(null);
-  const [tooltipDimensions, setTooltipDimensions] = useState<DOMRect | null>(
-    null,
-  );
-  const firstRender = useRef(true);
 
-  const spring: any = useSpring({
-    from: {
-      translate: [0, 0, 0],
-      opacity: 0,
-    },
-    to: async (next: any) => {
-      if (tooltipDimensions == null) {
-        return;
-      }
+  console.log('new one!!!');
 
-      const chartLeftBound = margin.Left;
-      const chartRightBound = chartDimensions.width - margin.Right;
+  const calculatedPosition = () => {
+    const tooltipDimensions =
+      tooltipRef == null || tooltipRef.current == null
+        ? null
+        : tooltipRef.current.getBoundingClientRect();
+    const tooltipWidth =
+      tooltipDimensions == null ? 0 : tooltipDimensions.width;
+    const tooltipHeight =
+      tooltipDimensions == null ? 0 : tooltipDimensions.height;
 
-      const naturalLeftBound =
-        currentX - tooltipDimensions.width - TOOLTIP_MARGIN;
-      const hasSpaceToLeft = naturalLeftBound > chartLeftBound;
+    const chartLeftBound = margin.Left;
+    const chartRightBound = chartDimensions.width - margin.Right;
 
-      const naturalRightBound =
-        currentX + tooltipDimensions.width + TOOLTIP_MARGIN;
-      const hasSpaceToRight = naturalRightBound < chartRightBound;
+    const naturalLeftBound = currentX - tooltipWidth - TOOLTIP_MARGIN;
+    const hasSpaceToLeft = naturalLeftBound > chartLeftBound;
 
-      let xTranslation = 0;
+    const naturalRightBound = currentX + tooltipWidth + TOOLTIP_MARGIN;
+    const hasSpaceToRight = naturalRightBound < chartRightBound;
 
-      if (position === 'center') {
-        xTranslation = clamp({
-          amount: currentX - tooltipDimensions.width / 2,
-          max: chartRightBound - tooltipDimensions.width,
-          min: chartLeftBound,
-        });
-      } else if (hasSpaceToLeft) {
-        xTranslation = naturalLeftBound;
-      } else if (hasSpaceToRight) {
-        xTranslation = currentX + TOOLTIP_MARGIN;
-      } else {
-        const centeredLeftBound = currentX - tooltipDimensions.width / 2;
-        const centeredRightBound = currentX + tooltipDimensions.width / 2;
+    let xTranslation = 0;
 
-        if (centeredRightBound > chartRightBound) {
-          xTranslation = chartRightBound - tooltipDimensions.width;
-        } else if (centeredLeftBound < chartLeftBound) {
-          xTranslation = chartLeftBound;
-        } else {
-          xTranslation = centeredLeftBound;
-        }
-      }
-
-      const shouldRenderImmediate = firstRender.current;
-      firstRender.current = false;
-
-      // react-spring docs do not return the `next` callback
-      // eslint-disable-next-line callback-return
-      await next({
-        translate: [
-          xTranslation,
-          Math.max(
-            margin.Top,
-            currentY - tooltipDimensions.height - TOOLTIP_MARGIN,
-          ),
-          0,
-        ],
-        opacity: 1,
-        immediate: shouldRenderImmediate,
+    if (position === 'center') {
+      xTranslation = clamp({
+        amount: currentX - tooltipWidth / 2,
+        max: chartRightBound - tooltipWidth,
+        min: chartLeftBound,
       });
-    },
-  });
+    } else if (hasSpaceToLeft) {
+      xTranslation = naturalLeftBound;
+    } else if (hasSpaceToRight) {
+      xTranslation = currentX + TOOLTIP_MARGIN;
+    } else {
+      const centeredLeftBound = currentX - tooltipWidth / 2;
+      const centeredRightBound = currentX + tooltipWidth / 2;
 
-  useEffect(() => {
-    if (tooltipRef.current == null) {
-      return;
+      if (centeredRightBound > chartRightBound) {
+        xTranslation = chartRightBound - tooltipWidth;
+      } else if (centeredLeftBound < chartLeftBound) {
+        xTranslation = chartLeftBound;
+      } else {
+        xTranslation = centeredLeftBound;
+      }
     }
 
-    setTooltipDimensions(tooltipRef.current.getBoundingClientRect());
-  }, [activePointIndex]);
+    return [
+      xTranslation,
+      Math.max(margin.Top, currentY - tooltipHeight - TOOLTIP_MARGIN),
+      0,
+    ];
+  };
+
+  const transform = calculatedPosition();
 
   return (
-    <animated.div
+    <div
       id={id}
       className={styles.Container}
       style={{
         top: 0,
         left: 0,
-        opacity: spring.opacity,
-        transform: spring.translate.interpolate(
-          // eslint-disable-next-line id-length
-          (x: number, y: number, z: number) =>
-            `translate3d(${x}px, ${y}px, ${z}px)`,
-        ),
+        transform: `translate3d(${transform[0]}px, ${transform[1]}px, ${transform[2]}px)`,
+        transition: 'transform 0.2s',
       }}
       ref={tooltipRef}
       aria-hidden="true"
     >
       {children}
-    </animated.div>
+    </div>
   );
 }
