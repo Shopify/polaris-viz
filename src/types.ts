d30e3bfc31c7870a8f533c50a93c6a0f0793854c
krystalcampioni <hello@krystalcampioni.com>
2021-05-17 11:39:10 -0400

    fade line chart area gradient

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,7 @@ export interface DataSeries<T, C> {
 export interface GradientStop {
   offset: number;
   color: string;
+  stopOpacity?: number;
 }
 
 export type SeriesColor = Color | GradientStop[];
