9c4c70d211db1e9011bbd5149d9c8b375fa27ae9
Nick Presta <nick.presta@shopify.com>
2021-06-09 09:27:57 -0400

    Fix linting

--- src/components/BarChart/index.ts
+++ src/components/BarChart/index.ts
@@ -1,9 +1,5 @@
 export {BarChart} from './BarChart';
 export type {BarChartProps} from './BarChart';
 
-export {
-  TooltipContent as BarChartTooltipContent,
-} from './components';
-export type {
-  TooltipContentProps as BarChartTooltipContentProps,
-} from './components';
+export {TooltipContent as BarChartTooltipContent} from './components';
+export type {TooltipContentProps as BarChartTooltipContentProps} from './components';
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,48 +3,24 @@ export {Bar} from './Bar';
 export {Crosshair} from './Crosshair';
 export {HorizontalGridLines} from './HorizontalGridLines';
 export {Sparkline} from './Sparkline';
-export type { SparklineProps} from './Sparkline';
+export type {SparklineProps} from './Sparkline';
 export {Sparkbar} from './Sparkbar';
 export type {SparkbarProps} from './Sparkbar';
-export {
-  BarChart,
-  BarChartTooltipContent,
-} from './BarChart';
-export type {
-  BarChartProps,
-  BarChartTooltipContentProps,
-} from './BarChart';
-export {
-  LineChart,
-  LineChartTooltipContent,
-} from './LineChart';
-export type {
-  LineChartProps,
-  LineChartTooltipContentProps,
-} from './LineChart';
-export {
-  NormalizedStackedBarChart,
-} from './NormalizedStackedBarChart';
-export type {
-  NormalizedStackedBarChartProps,
-} from './NormalizedStackedBarChart';
+export {BarChart, BarChartTooltipContent} from './BarChart';
+export type {BarChartProps, BarChartTooltipContentProps} from './BarChart';
+export {LineChart, LineChartTooltipContent} from './LineChart';
+export type {LineChartProps, LineChartTooltipContentProps} from './LineChart';
+export {NormalizedStackedBarChart} from './NormalizedStackedBarChart';
+export type {NormalizedStackedBarChartProps} from './NormalizedStackedBarChart';
 export {YAxis} from './YAxis';
 export {TooltipContainer} from './TooltipContainer';
-export {
-  SquareColorPreview,
-} from './SquareColorPreview';
-export type {
-  SquareColorPreviewProps,
-} from './SquareColorPreview';
+export {SquareColorPreview} from './SquareColorPreview';
+export type {SquareColorPreviewProps} from './SquareColorPreview';
 export {StackedAreaChart} from './StackedAreaChart';
 export type {StackedAreaChartProps} from './StackedAreaChart';
 export {LinearXAxis} from './LinearXAxis';
-export {
-  MultiSeriesBarChart,
-} from './MultiSeriesBarChart';
-export type {
-  MultiSeriesBarChartProps,
-} from './MultiSeriesBarChart';
+export {MultiSeriesBarChart} from './MultiSeriesBarChart';
+export type {MultiSeriesBarChartProps} from './MultiSeriesBarChart';
 export {TooltipContent} from './TooltipContent';
 export type {TooltipContentProps} from './TooltipContent';
 export {BarChartXAxis} from './BarChartXAxis';
