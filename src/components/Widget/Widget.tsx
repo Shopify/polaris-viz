db6cc074d35c091fc84a034a8e093e736849417d
Carys Mills <carys.mills@shopify.com>
2021-07-14 15:13:48 -0400

    WIP actually remove component

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,4 +1,4 @@
-import {useSpring, a} from '@react-spring/web';
+import {a, useTransition, config} from '@react-spring/web';
 import React, {useState} from 'react';
 
 import {Sparkbar} from '../../components';
@@ -8,52 +8,33 @@ import SaveMinor from './images/SaveMinor.svg';
 import styles from './Widget.scss';
 
 export function Widget() {
-  const [flipped, set] = useState(true);
-  const {transform, opacity} = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: {mass: 5, tension: 500, friction: 80},
+  const [frontVisible, setFrontVisible] = useState(true);
+  const transitions = useTransition(frontVisible, {
+    from: {
+      position: 'absolute',
+      opacity: 1,
+      transform: [180, 0],
+    },
+    enter: {opacity: 1, transform: [180, 0]},
+    leave: {opacity: 0, transform: [0, 180]},
+    // update: {opacity: 0, transform: [0, 180]},
+    // delay: 200,
+    config: config.molasses,
   });
 
-  return (
-    <div
-      style={{
-        width: 'min-content',
-        willChange: 'transform opacity',
-        position: 'absolute',
-        height: '200px',
-      }}
-    >
-      <a.div
-        className={styles.Card}
-        style={{opacity: opacity.to((opacity) => 1 - opacity), transform}}
-      >
-        <textarea
-          className={styles.TextArea}
-          value="SHOW total_sales FROM SALES SINCE -2w UNTIL today"
-        />
-
-        <div className={styles.ButtonContainer}>
-          <button
-            className={styles.Button}
-            onClick={() => set((state) => !state)}
-          >
-            <img width={18} height={18} src={SaveMinor} alt="save" />
-          </button>
-        </div>
-      </a.div>
-      <a.div
-        className={styles.Card}
+  function Front() {
+    return (
+      <div
         style={{
-          opacity,
-          transform,
-          rotateX: '180deg',
+          width: '250px',
+          willChange: 'transform opacity',
+          height: '200px',
         }}
       >
         <div className={styles.ButtonContainer}>
           <button
             className={styles.Button}
-            onClick={() => set((state) => !state)}
+            onClick={() => setFrontVisible((state) => !state)}
           >
             <img width={18} height={18} src={CodeMajor} alt="edit" />
           </button>
@@ -66,7 +47,6 @@ export function Widget() {
 
         <div style={{width: 250, height: 100}}>
           <Sparkbar
-            isAnimated
             data={[100, 200, 300, 400, 400, 1000, 200, 800, 900, 200, 400]}
             comparison={[
               {x: 0, y: 500},
@@ -84,7 +64,59 @@ export function Widget() {
             barFillStyle="gradient"
           />
         </div>
+      </div>
+    );
+  }
+
+  function Back() {
+    return (
+      <div
+        style={{
+          willChange: 'transform opacity',
+          height: '200px',
+          width: '250px',
+        }}
+      >
+        <textarea
+          className={styles.TextArea}
+          value="SHOW total_sales FROM SALES SINCE -2w UNTIL today"
+        />
+
+        <div className={styles.ButtonContainer}>
+          <button
+            className={styles.Button}
+            onClick={() => setFrontVisible((state) => !state)}
+          >
+            <img width={18} height={18} src={SaveMinor} alt="save" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return transitions(({opacity, transform}, frontState) =>
+    frontState ? (
+      <a.div
+        style={{
+          position: 'absolute',
+          opacity: opacity.to({range: [0.0, 1.0], output: [0, 1]}),
+          background: 'red',
+          transform: transform.to((x, y) => `rotateX(${y}deg)`),
+        }}
+      >
+        <Front />
       </a.div>
-    </div>
+    ) : (
+      <a.div
+        style={{
+          position: 'absolute',
+          opacity: opacity.to({range: [1.0, 0.0], output: [1, 0]}),
+          background: 'purple',
+          transform: transform.to((x, y) => `rotateX(${y}deg)`),
+        }}
+      >
+        <Back />
+      </a.div>
+    ),
   );
 }
