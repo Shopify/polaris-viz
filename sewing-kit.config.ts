f5f1297d530875ab04c792b736cc07e3a88abf44
Nick Presta <nick.presta@shopify.com>
2021-09-13 15:46:26 -0400

    Upgrade sewing-kit to 4.0.0
    Replace our custom browserslist configuration with the standard Shopify
    configuration to match what we do in other projects.
    
    Also, updates the browserslist resolution to ^4.17 since this is what
    sewing-kit needs and we can't remove the resolution (yet) because other
    packages still depend on older versions, which are vulnerable to
    security issue(s).

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -10,6 +10,12 @@ export default function sewingKitConfig(
     name: 'polaris-viz',
     library: true,
     plugins: [
+      plugins.dupeCheck((defaults) => ({
+        ...defaults,
+        // @size-limit/time depends on react@17.0.2
+        // which is only used in a GitHub action, so we don't care that it's duplicated
+        allowlist: new Map([['react', ['16.12.0', '17.0.2']]]),
+      })),
       plugins.jest((config) => {
         config.roots = [join(__dirname, 'src'), join(__dirname, 'tests')];
 
