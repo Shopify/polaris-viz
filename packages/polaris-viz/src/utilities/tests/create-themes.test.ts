7bba24bc78fcb80e44a8ec6f9f7769e82a442e52
Philipp Schöfer <philipp.schofer@shopify.com>
2022-01-25 10:48:24 -0500

    Renamed `web` package `polaris-viz`

---
+++ packages/polaris-viz/src/utilities/tests/create-themes.test.ts
@@ -0,0 +1,71 @@
+import {createTheme, createThemes} from '../';
+import {DEFAULT_THEME} from '../../constants';
+
+describe('createTheme', () => {
+  it('generates a theme with default values, from the partial theme provided', () => {
+    const result = createTheme({
+      bar: {
+        innerMargin: 'Small',
+      },
+    });
+    expect(result).not.toStrictEqual(DEFAULT_THEME);
+
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        bar: {
+          ...DEFAULT_THEME.bar,
+          innerMargin: 'Small',
+        },
+      }),
+    );
+  });
+});
+
+describe('createThemes', () => {
+  it('generates a record of themes with default values, from the partial themes provided', () => {
+    const result = createThemes({
+      Default: {
+        bar: {
+          innerMargin: 'Small',
+        },
+      },
+    });
+    expect(result).not.toStrictEqual({Default: DEFAULT_THEME});
+
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        Default: {
+          ...DEFAULT_THEME,
+          bar: {
+            ...DEFAULT_THEME.bar,
+            innerMargin: 'Small',
+          },
+        },
+      }),
+    );
+  });
+
+  it('generates a record with multiple custom themes', () => {
+    const result = createThemes({
+      Default: DEFAULT_THEME,
+      SomeTheme: {
+        bar: {
+          hasRoundedCorners: false,
+        },
+      },
+    });
+
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        Default: DEFAULT_THEME,
+        SomeTheme: {
+          ...DEFAULT_THEME,
+          bar: {
+            ...DEFAULT_THEME.bar,
+            hasRoundedCorners: false,
+          },
+        },
+      }),
+    );
+  });
+});
