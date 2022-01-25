620c5902e18abe8cda332dc0d844582cb2082810
Philipp Schöfer <philipp.schofer@shopify.com>
2022-01-25 10:48:24 -0500

    Renamed `web` package `polaris-viz`

---
+++ packages/polaris-viz/src/hooks/tests/usePolarisVizContext.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import {mount} from '@shopify/react-testing';
+
+import {mountWithProvider} from '../../test-utilities';
+import {
+  DEFAULT_THEME,
+  LIGHT_THEME,
+  PRINT_THEME,
+} from '../../../../polaris-viz-core/src';
+import {usePolarisVizContext} from '../usePolarisVizContext';
+
+describe('usePolarisVizContext', () => {
+  function TestComponent() {
+    const {themes} = usePolarisVizContext();
+    return <div>{JSON.stringify(themes)}</div>;
+  }
+
+  it('exposes the default theme if used without a parent PolarisVizProvider', () => {
+    const mockComponent = mount(<TestComponent />);
+
+    expect(mockComponent.text()).toBe(
+      JSON.stringify({
+        Default: DEFAULT_THEME,
+        Light: LIGHT_THEME,
+      }),
+    );
+  });
+
+  it('exposes the default values overwritten by PolarisVizProvider', () => {
+    const mockComponent = mountWithProvider(<TestComponent />, {
+      themes: {
+        Default: {
+          chartContainer: {
+            backgroundColor: 'purple',
+          },
+        },
+      },
+    });
+
+    expect(mockComponent.text()).toBe(
+      JSON.stringify({
+        Default: {
+          ...DEFAULT_THEME,
+          chartContainer: {
+            ...DEFAULT_THEME.chartContainer,
+            backgroundColor: 'purple',
+          },
+        },
+        Light: LIGHT_THEME,
+        Print: PRINT_THEME,
+      }),
+    );
+  });
+
+  it('exposes custom themes created in PolarisVizProvider', () => {
+    const mockComponent = mountWithProvider(<TestComponent />, {
+      themes: {
+        SomeOtherTheme: {
+          chartContainer: {
+            backgroundColor: 'purple',
+          },
+        },
+      },
+    });
+
+    expect(mockComponent.text()).toBe(
+      JSON.stringify({
+        Default: DEFAULT_THEME,
+        Light: LIGHT_THEME,
+        Print: PRINT_THEME,
+        SomeOtherTheme: {
+          ...DEFAULT_THEME,
+          chartContainer: {
+            ...DEFAULT_THEME.chartContainer,
+            backgroundColor: 'purple',
+          },
+        },
+      }),
+    );
+  });
+});
---
+++ packages/polaris-viz/src/utilities/polaris-viz-context.ts
@@ -0,0 +1,13 @@
+import {createContext} from 'react';
+
+import type {Theme} from '../types';
+import {DEFAULT_THEME as Default, LIGHT_THEME as Light} from '../constants';
+
+export const PolarisVizContext = createContext<{
+  themes: {[key: string]: Theme};
+}>({
+  themes: {
+    Default,
+    Light,
+  },
+});
