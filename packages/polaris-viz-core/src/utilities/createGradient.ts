5db69ec9b4eddbbff21b6a2b4a828929f75ac435
Matt Vickers <matthew.vickers@shopify.com>
2022-02-02 13:30:02 -0600

    Document core utilities

--- packages/polaris-viz-core/src/utilities/createGradient.ts
+++ packages/polaris-viz-core/src/utilities/createGradient.ts
@@ -1,4 +1,9 @@
-export const createGradient = (color1: string, color2: string) => {
+import type {GradientStop} from 'types';
+
+export const createGradient = (
+  color1: string,
+  color2: string,
+): GradientStop[] => {
   return [
     {offset: 0, color: color2},
     {offset: 100, color: color1},
