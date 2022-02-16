d069f88b79411c7d8d9cb1a5e8e4514948f9c64b
Philipp Schöfer <philipp.schofer@shopify.com>
2022-02-16 14:56:05 -0500

    Removes unused imports in `polaris-viz/src/index.tsx`

--- packages/polaris-viz/src/index.tsx
+++ packages/polaris-viz/src/index.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import {PolarisVizProvider as OriginalPolarisVizProvider} from '@shopify/polaris-viz-core';
-import {animated} from '@react-spring/web';
-
 export {
   SparkLineChart,
   SparkBarChart,
