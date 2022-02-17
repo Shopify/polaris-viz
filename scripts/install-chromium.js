cee9861383ae0604bac995e0df18ee93a820fe2e
Carys Mills <carys.mills@shopify.com>
2022-02-17 12:50:10 -0500

    remove size limit

--- scripts/install-chromium.js
+++
@@ -1,7 +0,0 @@
-const {execSync} = require('child_process');
-
-execSync('sudo yarn add puppeteer --unsafe-perm=true --allow-root');
-
-execSync('node ./node_modules/puppeteer/install.js');
-
-console.log('added puppeteer')
