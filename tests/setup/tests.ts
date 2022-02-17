4b7db0992e490e6ca1a3909c53efac7b376fae7b
Philipp Schöfer <philipp.schofer@shopify.com>
2022-02-17 12:16:14 -0500

    Adds tests to ensure changelog format and consistency overall

--- tests/setup/tests.ts
+++ tests/setup/tests.ts
@@ -31,27 +31,29 @@ jest.mock('../../packages/polaris-viz/src/constants.ts', () => {
   };
 });
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
+if (typeof window !== 'undefined') {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 
-Object.defineProperty(window, 'ResizeObserver', {
-  value: jest.fn(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-  })),
-});
+  Object.defineProperty(window, 'ResizeObserver', {
+    value: jest.fn(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    })),
+  });
+}
 
 Globals.assign({
   skipAnimation: true,
