381442171e774a7a3e0ae623a56452572cf062be
Carys Mills <carys.mills@shopify.com>
2022-02-24 15:15:27 -0500

    Add react native demo - WIP

--- packages/polaris-viz-native/src/components/ChartContainer/ChartContainer.tsx
+++ packages/polaris-viz-native/src/components/ChartContainer/ChartContainer.tsx
@@ -1,30 +1,30 @@
-import React, {useState, ReactElement, cloneElement} from 'react';
-import {View} from 'react-native';
-import type {Dimensions} from '@shopify/polaris-viz-core';
-import {useTheme} from '@shopify/polaris-viz-core';
+import React, { useState, ReactElement, cloneElement } from 'react';
+import { View } from 'react-native';
+import type { Dimensions } from '@shopify/polaris-viz-core';
+import { useTheme } from '@shopify/polaris-viz-core';
 
 interface Props {
   children: ReactElement;
   theme?: string;
 }
 
-export function ChartContainer({theme, children}: Props) {
+export function ChartContainer({ theme, children }: Props) {
   const [chartDimensions, setChartDimensions] =
     useState<Dimensions | null>(null);
 
-  const {chartContainer} = useTheme(theme);
+  const { chartContainer } = useTheme(theme);
 
   const handleOnLayout = (event) => {
-    const {width, height} = event.nativeEvent.layout;
-    setChartDimensions({width, height});
+    const { width, height } = event.nativeEvent.layout;
+    setChartDimensions({ width, height });
   };
   return (
     <View
       onLayout={handleOnLayout}
       style={[
         {
-          borderRadius: Number(chartContainer.borderRadius),
-          padding: chartContainer.padding,
+          borderRadius: Number(0),
+          padding: Number(1),
           backgroundColor: chartContainer.backgroundColor,
         },
         {
@@ -37,9 +37,9 @@ export function ChartContainer({theme, children}: Props) {
       {chartDimensions == null
         ? null
         : cloneElement(children, {
-            theme,
-            dimensions: chartDimensions,
-          })}
+          theme,
+          dimensions: chartDimensions,
+        })}
     </View>
   );
 }
