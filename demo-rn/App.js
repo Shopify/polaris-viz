8233249af89bdcf312dc71c99aa7467833d1930a
Philipp Schöfer <philipp.schofer@shopify.com>
2022-02-25 14:10:42 -0500

    Re-Added reanimated

--- demo-rn/App.js
+++ demo-rn/App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 
+import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {useColorScheme} from 'react-native';
