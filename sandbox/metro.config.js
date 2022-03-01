72e289b58b43284f303fc326f4885d0afe30ca13
krystalcampioni <hello@krystalcampioni.com>
2022-03-01 13:52:05 -0500

    re-add sandbox folder

---
+++ sandbox/metro.config.js
@@ -0,0 +1,20 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const path = require('path');
+
+const {getDefaultConfig} = require('expo/metro-config');
+
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, '../');
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
