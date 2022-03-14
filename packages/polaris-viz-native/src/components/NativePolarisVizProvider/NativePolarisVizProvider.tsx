e647e3947f95f921de0d4aa777a7b7648f85a14c
krystalcampioni <hello@krystalcampioni.com>
2022-03-14 17:08:19 -0400

    optional themes on providers

--- packages/polaris-viz-native/src/components/NativePolarisVizProvider/NativePolarisVizProvider.tsx
+++ packages/polaris-viz-native/src/components/NativePolarisVizProvider/NativePolarisVizProvider.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {PolarisVizProvider as OriginalPolarisVizProvider} from '@shopify/polaris-viz-core';
 import {animated} from '@react-spring/native';
+import type {PartialTheme} from '@shopify/polaris-viz-core';
 
 import {DEFAULT_COMPONENTS as NativeComponents} from '../../constants';
 
-export const NativePolarisVizProvider = ({themes, children}) => {
+export const NativePolarisVizProvider = ({
+  themes,
+  children,
+}: {
+  children: React.ReactNode;
+  themes?: {[key: string]: PartialTheme};
+}) => {
   return (
     <OriginalPolarisVizProvider
       themes={themes}
--- packages/polaris-viz/src/components/WebPolarisVizProvider/WebPolarisVizProvider.tsx
+++ packages/polaris-viz/src/components/WebPolarisVizProvider/WebPolarisVizProvider.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import {PolarisVizProvider as OriginalPolarisVizProvider} from '@shopify/polaris-viz-core';
 import {animated} from '@react-spring/web';
+import type {PartialTheme} from '@shopify/polaris-viz-core';
 
-export const WebPolarisVizProvider = ({themes, children}) => {
+export const WebPolarisVizProvider = ({
+  themes,
+  children,
+}: {
+  children: React.ReactNode;
+  themes?: {[key: string]: PartialTheme};
+}) => {
   return (
     <OriginalPolarisVizProvider themes={themes} animated={animated}>
       {children}
