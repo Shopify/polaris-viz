719af0bf479c709a84ebe674af0f24ee799f0d55
Philipp Schöfer <philipp.schofer@shopify.com>
2022-03-23 15:30:10 -0400

    Adds post-publish message

---
+++ scripts/postpublish.js
@@ -0,0 +1,6 @@
+process.stdout.write(
+  `Versioned for all packages were bumped. Please use "git push origin main --follow-tags" to push your changes to main.`,
+);
+process.stdout.write(
+  `\n\nNext Step: Visit https://shipit.shopify.io/shopify/polaris-viz/production and click "Deploy" once CI is ✅\n`,
+);
