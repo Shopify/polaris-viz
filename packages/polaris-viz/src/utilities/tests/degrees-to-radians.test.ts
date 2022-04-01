35250b1be8c425fdf94999bda271371b220954c9
Matt Vickers <matthew.vickers@shopify.com>
2022-04-01 15:04:24 -0500

    Remove unused methods

--- packages/polaris-viz/src/utilities/tests/degrees-to-radians.test.ts
+++
@@ -1,8 +0,0 @@
-import {degreesToRadians} from '../degrees-to-radians';
-
-describe('degreesToRadians', () => {
-  it('returns a radian value', () => {
-    const actual = degreesToRadians(40);
-    expect(actual).toBe(0.6981317007977318);
-  });
-});
