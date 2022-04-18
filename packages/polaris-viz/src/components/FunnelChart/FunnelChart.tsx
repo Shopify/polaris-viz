a81072b64572dfcd0b97875ad2ba51df745760b8
Juwan Petty <juwan.petty@shopify.com>
2022-04-18 11:45:58 -0400

    Adds Bar component to display bars in FunnelChart

--- packages/polaris-viz/src/components/FunnelChart/FunnelChart.tsx
+++ packages/polaris-viz/src/components/FunnelChart/FunnelChart.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import type {DataSeries} from '@shopify/polaris-viz-core';
 
+import {useTheme, useThemeSeriesColors} from '../../hooks';
 import {ChartContainer} from '../../components/ChartContainer';
+
+import {Chart} from './Chart';
 
 export interface FunnelChartProps {
   data: DataSeries[];
   theme?: string;
 }
 
-export function FunnelChart({theme}: FunnelChartProps) {
+export function FunnelChart({theme = 'Default', data}: FunnelChartProps) {
+  const selectedTheme = useTheme(theme);
+  const seriesColors = useThemeSeriesColors(data, selectedTheme);
+
+  const seriesWithDefaults = data.map((series, index) => ({
+    color: seriesColors[index],
+    ...series,
+  }));
+
   return (
-    <React.Fragment>
-      <ChartContainer theme={theme}>
-        <svg />
-      </ChartContainer>
-    </React.Fragment>
+    <ChartContainer theme={theme}>
+      <Chart data={seriesWithDefaults} />
+    </ChartContainer>
   );
 }
