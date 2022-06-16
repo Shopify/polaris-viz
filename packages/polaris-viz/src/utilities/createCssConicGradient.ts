187a1bfded8e56ef4d8ef055b1945d944f9a2d51
Logan Wright <logan.wright@shopify.com>
2022-06-16 16:25:09 -0500

    implements DonutChart

--- packages/polaris-viz/src/utilities/createCssConicGradient.ts
+++ packages/polaris-viz/src/utilities/createCssConicGradient.ts
@@ -1,11 +1,14 @@
 import type {GradientStop} from '@shopify/polaris-viz-core';
 
 export function createCSSConicGradient(gradient: GradientStop[]) {
+<<<<<<< HEAD
   if (gradient.length === 1) {
     const color = gradient[0].color;
     return `conic-gradient(${color} 0%, ${color} 100%)`;
   }
 
+=======
+>>>>>>> c278f684 (implements DonutChart)
   const gradientStops = gradient.map(
     ({color, offset}) => `${color} ${offset}%`,
   );
