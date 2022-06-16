100c877eb352a612b7d70934c857046d7d990626
Logan Wright <logan.wright@shopify.com>
2022-06-16 16:37:23 -0500

    adds tests to DonutChart

--- packages/polaris-viz/src/components/ConicGradientWithStops/tests/ConicGradientWithStops.test.tsx
+++ packages/polaris-viz/src/components/ConicGradientWithStops/tests/ConicGradientWithStops.test.tsx
@@ -4,6 +4,7 @@ import {mount} from '@shopify/react-testing';
 import {ConicGradientWithStops} from '..';
 import type {ConicGradientWithStopsProps} from '../ConicGradientWithStops';
 
+<<<<<<< HEAD
 const mockProps1 = {
   height: 200,
   width: 200,
@@ -83,6 +84,28 @@ describe('<ConicGradientWithStops />', () => {
 
     expect(conicGradient).toContainReactComponent('div', {
       style,
+=======
+describe('<ConicGradientWithStops />', () => {
+  const mockProps: ConicGradientWithStopsProps = {
+    height: 200,
+    width: 200,
+    gradient: [
+      {color: 'lime', offset: 0},
+      {color: 'magenta', offset: 100},
+    ],
+  };
+  const conicGradientValue = 'conic-gradient(lime 0%, magenta 100%)';
+
+  it('renders a div with the given styles', () => {
+    const conicGradient = mount(<ConicGradientWithStops {...mockProps} />);
+
+    expect(conicGradient).toContainReactComponent('div', {
+      style: {
+        height: `${mockProps.height}px`,
+        width: `${mockProps.width}px`,
+        backgroundImage: conicGradientValue,
+      },
+>>>>>>> 4415e3c7 (adds tests to DonutChart)
     });
   });
 });
