f405fb89562ad5d581acc4dcb609ace8b2c90f7e
Logan Wright <logan.wright@shopify.com>
2022-06-17 09:49:02 -0500

    adds it.each tests to gradients

--- packages/polaris-viz/src/components/ConicGradientWithStops/tests/ConicGradientWithStops.test.tsx
+++ packages/polaris-viz/src/components/ConicGradientWithStops/tests/ConicGradientWithStops.test.tsx
@@ -4,26 +4,84 @@ import {mount} from '@shopify/react-testing';
 import {ConicGradientWithStops} from '..';
 import type {ConicGradientWithStopsProps} from '../ConicGradientWithStops';
 
-describe('<ConicGradientWithStops />', () => {
-  const mockProps: ConicGradientWithStopsProps = {
-    height: 200,
-    width: 200,
-    gradient: [
-      {color: 'lime', offset: 0},
-      {color: 'magenta', offset: 100},
-    ],
-  };
-  const conicGradientValue = 'conic-gradient(lime 0%, magenta 100%)';
+const mockProps1 = {
+  height: 200,
+  width: 200,
+  gradient: [
+    {color: 'lime', offset: 0},
+    {color: 'magenta', offset: 1},
+  ],
+};
+const mockProps2 = {
+  height: 200,
+  width: 200,
+  gradient: [
+    {color: 'red', offset: 50},
+    {color: 'green', offset: 75},
+  ],
+};
+const mockProps3 = {
+  height: 200,
+  width: 200,
+  gradient: [
+    {color: 'purple', offset: 12},
+    {color: 'orange', offset: 12},
+  ],
+};
+const mockProps4 = {
+  height: 200,
+  width: 200,
+  gradient: [
+    {color: 'black', offset: 0},
+    {color: 'white', offset: 166},
+  ],
+};
 
-  it('renders a div with the given styles', () => {
-    const conicGradient = mount(<ConicGradientWithStops {...mockProps} />);
+const result1 = {
+  height: `${mockProps1.height}px`,
+  width: `${mockProps1.width}px`,
+  backgroundImage: 'conic-gradient(lime 0%, magenta 1%)',
+};
+const result2 = {
+  height: `${mockProps2.height}px`,
+  width: `${mockProps2.width}px`,
+  backgroundImage: 'conic-gradient(red 50%, green 75%)',
+};
+
+const result3 = {
+  height: `${mockProps3.height}px`,
+  width: `${mockProps3.width}px`,
+  backgroundImage: 'conic-gradient(purple 12%, orange 12%)',
+};
+const result4 = {
+  height: `${mockProps4.height}px`,
+  width: `${mockProps4.width}px`,
+  backgroundImage: 'conic-gradient(black 0%, white 166%)',
+};
+
+describe('<ConicGradientWithStops />', () => {
+  it.each([
+    {
+      props: mockProps1,
+      style: result1,
+    },
+    {
+      props: mockProps2,
+      style: result2,
+    },
+    {
+      props: mockProps3,
+      style: result3,
+    },
+    {
+      props: mockProps4,
+      style: result4,
+    },
+  ])('renders a div with the given styles', ({props, style}) => {
+    const conicGradient = mount(<ConicGradientWithStops {...props} />);
 
     expect(conicGradient).toContainReactComponent('div', {
-      style: {
-        height: `${mockProps.height}px`,
-        width: `${mockProps.width}px`,
-        backgroundImage: conicGradientValue,
-      },
+      style,
     });
   });
 });
--- packages/polaris-viz/src/utilities/tests/createCssConicGradient.test.ts
+++ packages/polaris-viz/src/utilities/tests/createCssConicGradient.test.ts
@@ -1,16 +1,52 @@
 import {createCSSConicGradient} from '../createCssConicGradient';
 
-const GRADIENT_STOP = [
+const gradientStop1 = [
   {color: 'lime', offset: 0},
   {color: 'magenta', offset: 1},
 ];
 
-const CONIC_GRADIENT = 'conic-gradient(lime 0%, magenta 1%)';
+const gradientStop2 = [
+  {color: 'red', offset: 50},
+  {color: 'green', offset: 75},
+];
+const gradientStop3 = [
+  {color: 'purple', offset: 12},
+  {color: 'orange', offset: 12},
+];
+const gradientStop4 = [
+  {color: 'black', offset: 0},
+  {color: 'white', offset: 166},
+];
+
+const result1 = 'conic-gradient(lime 0%, magenta 1%)';
+const result2 = 'conic-gradient(red 50%, green 75%)';
+const result3 = 'conic-gradient(purple 12%, orange 12%)';
+const result4 = 'conic-gradient(black 0%, white 166%)';
 
 describe('createCssConicGradient', () => {
-  it('returns a conic gradient string with given values', () => {
-    const conicGradient = createCSSConicGradient(GRADIENT_STOP);
+  it.each([
+    {
+      gradientStop: gradientStop1,
+      expected: result1,
+    },
+    {
+      gradientStop: gradientStop2,
+      expected: result2,
+    },
+    {
+      gradientStop: gradientStop3,
+      expected: result3,
+    },
+    {
+      gradientStop: gradientStop4,
+      expected: result4,
+    },
+  ])(
+    'creates a conic-gradient string with the given gradientStop',
+    ({gradientStop, expected}) => {
+      const conicGradient = createCSSConicGradient(gradientStop);
 
-    expect(conicGradient).toBe(CONIC_GRADIENT);
-  });
+      expect(conicGradient).toStrictEqual(expected);
+    },
+  );
 });
