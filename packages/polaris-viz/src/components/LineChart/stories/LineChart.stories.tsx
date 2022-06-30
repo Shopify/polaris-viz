168d0731fbd1248fefbd3499e911193d51665ed1
Margarita Morozova <margarita.morozova@shopify.com>
2022-06-30 13:53:04 -0700

    fixes build error

--- packages/polaris-viz/src/components/LineChart/stories/LineChart.stories.tsx
+++ packages/polaris-viz/src/components/LineChart/stories/LineChart.stories.tsx
@@ -235,6 +235,7 @@ const ANNOTATIONS: Annotation[] = [
   {
     startKey: '2020-04-02T12:00:00',
     label: 'Sales increase',
+    axis: 'x'
   },
   {
     startKey: '2020-04-06T12:00:00',
@@ -242,6 +243,7 @@ const ANNOTATIONS: Annotation[] = [
     content: {
       content: 'We ran a massive sale on our products. We made a lot of money!',
     },
+    axis: 'x'
   },
 ];
 
