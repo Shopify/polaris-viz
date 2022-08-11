0254ec7b0793fa97be8bb3bf760d97cb018860aa
Matt Vickers <matthew.vickers@shopify.com>
2022-08-11 09:34:04 -0500

    Hide error when running tests

--- packages/polaris-viz/src/components/ChartErrorBoundary/tests/ChartErrorBoundary.test.tsx
+++ packages/polaris-viz/src/components/ChartErrorBoundary/tests/ChartErrorBoundary.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import {ChartSkeleton, SkeletonType} from '../../ChartSkeleton';
+import {ChartSkeleton} from '../../ChartSkeleton';
 import {ChartErrorBoundary} from '../ChartErrorBoundary';
-import {
-  mountWithProvider,
-  mockDefaultTheme,
-} from '../../../test-utilities/mountWithProvider';
+import {mountWithProvider} from '../../../test-utilities/mountWithProvider';
 
 const MOCK_PROPS = {
   dimensions: {
@@ -19,6 +16,21 @@ function Child() {
 }
 
 describe('<ChartErrorBoundary />', () => {
+  // eslint-disable-next-line no-console
+  const realError = console.error;
+
+  beforeAll(() => {
+    // Remove the error message from the console because
+    // we're expected to have an error.
+    // eslint-disable-next-line no-console
+    console.error = (...x) => {};
+  });
+
+  afterAll(() => {
+    // eslint-disable-next-line no-console
+    console.error = realError;
+  });
+
   describe('<ChartSkeleton />', () => {
     it('is visible when an error occurs', () => {
       const wrapper = mountWithProvider(
