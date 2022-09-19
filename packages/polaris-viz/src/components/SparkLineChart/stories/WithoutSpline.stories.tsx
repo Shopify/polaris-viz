207bbd147fa2a3c574b74e4ffdd12a01f0a33a6e
Matt Vickers <matthew.vickers@shopify.com>
2022-09-19 08:23:06 -0500

    Render dot in correct position when non-numeric keys provided

--- packages/polaris-viz/src/components/SparkLineChart/stories/WithoutSpline.stories.tsx
+++ packages/polaris-viz/src/components/SparkLineChart/stories/WithoutSpline.stories.tsx
@@ -4,11 +4,12 @@ export {META as default} from './meta';
 
 import type {SparkLineChartProps} from '../../../components';
 
-import {DEFAULT_PROPS, Template} from './data';
+import {DEFAULT_DATA, DEFAULT_PROPS, Template} from './data';
 
 export const withoutSpline: Story<SparkLineChartProps> = Template.bind({});
 
 withoutSpline.args = {
   ...DEFAULT_PROPS,
+  data: DEFAULT_DATA,
   theme: 'NoSpline',
 };
---
+++ packages/polaris-viz/src/components/SparkLineChart/stories/playground/NonNumeric.chromatic.stories.tsx
@@ -0,0 +1,32 @@
+import type {Story} from '@storybook/react';
+
+import {META} from '../meta';
+
+export default {
+  ...META,
+  title: `${META.title}/Playground`,
+};
+
+import type {SparkLineChartProps} from '../../../../components';
+
+import {DEFAULT_PROPS, Template} from '../data';
+
+export const NonNumericKeys: Story<SparkLineChartProps> = Template.bind({});
+
+NonNumericKeys.args = {
+  ...DEFAULT_PROPS,
+  data: [
+    {
+      name: 'Sales',
+      data: [
+        {value: 100, key: '2020-04-01T12:00:00'},
+        {value: 99, key: '2020-04-02T12:00:00'},
+        {value: 1000, key: '2020-04-03T12:00:00'},
+        {value: 2, key: '2020-04-04T12:00:00'},
+        {value: 22, key: '2020-04-05T12:00:00'},
+        {value: 6, key: '2020-04-06T12:00:00'},
+        {value: 5, key: '2020-04-07T12:00:00'},
+      ],
+    },
+  ],
+};
