69d00999c99d0be98275201257924defa0d037c5
Andrew Zulaybar <andrew.zulaybar@shopify.com>
2022-11-29 22:21:07 +0000

    Add support for custom legend in StackedAreaChart

---
+++ packages/polaris-viz/src/components/StackedAreaChart/stories/InteractiveCustomLegend.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type {Story} from '@storybook/react';
+
+export {META as default} from './meta';
+
+import {DEFAULT_THEME} from '../../../constants';
+import {LinePreview} from '../../LinePreview';
+import type {StackedAreaChartProps} from '../../StackedAreaChart';
+
+import {DEFAULT_DATA, DEFAULT_PROPS, Template} from './data';
+
+const liStyles = {
+  alignItems: 'center',
+  color: 'white',
+  display: 'flex',
+  fontWeight: 'bold',
+  gap: 4,
+  listStyleType: 'none',
+};
+
+export const InteractiveCustomLegend: Story<StackedAreaChartProps> =
+  Template.bind({});
+
+InteractiveCustomLegend.args = {
+  ...DEFAULT_PROPS,
+  data: DEFAULT_DATA,
+  renderLegendContent: ({getColorVisionStyles, getColorVisionEventAttrs}) => (
+    <ul>
+      {DEFAULT_DATA.map(({name}, index) => (
+        <li
+          key={name}
+          style={{
+            ...liStyles,
+            ...getColorVisionStyles(index),
+          }}
+          {...getColorVisionEventAttrs(index)}
+        >
+          <LinePreview
+            lineStyle="solid"
+            color={DEFAULT_THEME.seriesColors.upToFour[index]}
+          />
+          {name}
+        </li>
+      ))}
+    </ul>
+  ),
+};
