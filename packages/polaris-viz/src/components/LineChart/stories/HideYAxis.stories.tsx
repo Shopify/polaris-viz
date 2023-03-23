fb5f538e8755ddce8e56a7c2452643e8e255c716
Virak Ngauv <virak.ngauv@shopify.com>
2023-03-23 19:03:43 +0000

    more pairing again

--- packages/polaris-viz/src/components/LineChart/stories/HideYAxis.stories.tsx
+++ packages/polaris-viz/src/components/LineChart/stories/HideYAxis.stories.tsx
@@ -1,37 +1,12 @@
-import {PolarisVizProvider} from '../../PolarisVizProvider';
 import type {Story} from '@storybook/react';
 
 export {META as default} from './meta';
 
-import {LineChart, LineChartProps} from '../..';
+import type {LineChartProps} from '../..';
 
-import {DEFAULT_DATA, DEFAULT_PROPS} from './data';
+import {DEFAULT_DATA, DEFAULT_PROPS, Template} from './data';
 
-const HideYAxisTemplate: Story<LineChartProps> = (args: LineChartProps) => {
-  const themeOverrides = {
-    chartContainer: {
-      // padding: '0px',
-    },
-    grid: {
-      // showHorizontalLines: false,
-      horizontalMargin: 0, //controls right gap
-      horizontalOverflow: false,
-    },
-
-  }
-  return (
-    <PolarisVizProvider
-      themes={{
-        Default: themeOverrides,
-        Light: themeOverrides,
-      }}
-    >
-      <LineChart {...args} />
-    </PolarisVizProvider>
-  )
-};
-
-export const HideYAxis: Story<LineChartProps> = HideYAxisTemplate.bind({});
+export const HideYAxis: Story<LineChartProps> = Template.bind({});
 
 HideYAxis.args = {
   ...DEFAULT_PROPS,
