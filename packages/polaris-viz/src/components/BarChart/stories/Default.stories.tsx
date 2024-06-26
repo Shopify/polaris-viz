51c2342089a34b7ec63665da485f1f016674da89
Matt Vickers <matthew.vickers@shopify.com>
2024-06-26 15:09:09 -0400

    We be formatting

--- packages/polaris-viz/src/components/BarChart/stories/Default.stories.tsx
+++ packages/polaris-viz/src/components/BarChart/stories/Default.stories.tsx
@@ -8,8 +8,63 @@ import {DEFAULT_DATA, Template} from './data';
 
 export const Default: Story<BarChartProps> = Template.bind({});
 
+const data = [
+  {
+    name: 'Monday',
+    data: [
+      {key: 'Breakfast', value: 3},
+      {key: 'Lunch', value: 4},
+      {key: 'Dinner', value: 7},
+    ],
+  },
+  {
+    name: 'Tuesday',
+    data: [
+      {key: 'Lunch', value: 14},
+      {key: 'Dinner', value: 17},
+    ],
+    color: 'red',
+  },
+  {
+    name: 'Wednesday',
+    data: [
+      {key: 'Breakfast', value: 23},
+      {key: 'Dinner', value: 27},
+    ],
+  },
+  {
+    name: 'Thursday',
+    data: [
+      {key: 'Breakfast', value: 33},
+      {key: 'Lunch', value: 34},
+    ],
+  },
+];
+
+function formatData(data) {
+  const keys = new Set();
+
+  for (const series of data) {
+    for (const {key} of series.data) {
+      keys.add(key);
+    }
+  }
+
+  return [...keys].map((key, index) => {
+    return {
+      ...data[index],
+      name: key,
+      data: data.map((series) => {
+        const values = series.data.find((d) => d.key === key);
+
+        return {key: series.name ?? '', value: values?.value ?? 0};
+      }),
+    };
+  });
+}
+
 Default.args = {
-  data: DEFAULT_DATA,
+  data: formatData(data),
   onError: (a, b) => {
     console.log({a, b});
   },
