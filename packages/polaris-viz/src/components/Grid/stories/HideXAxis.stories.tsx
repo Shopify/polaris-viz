5281e5f8724670d189292716db27bdfb0c6fed8a
Jorge Moller <jmoller2106@gmail.com>
2024-09-03 14:37:38 -0300

    Hide x axis and y axis

--- packages/polaris-viz/src/components/Grid/stories/HideXAxis.stories.tsx
+++ packages/polaris-viz/src/components/Grid/stories/HideXAxis.stories.tsx
@@ -2,12 +2,13 @@ import type {Story} from '@storybook/react';
 
 export {META as default} from './meta';
 
-import {Grid, type GridProps} from '../Grid';
 import {CELL_GROUPS, DEFAULT_DATA, Template} from './data';
+import {GridProps} from '../Grid';
 
-export const Default: Story<GridProps> = Template.bind({});
+export const HideXAxis: Story<GridProps> = Template.bind({});
 
-Default.args = {
+HideXAxis.args = {
   data: DEFAULT_DATA,
   cellGroups: CELL_GROUPS,
+  xAxisOptions: {hide: true},
 };
--- packages/polaris-viz/src/components/Grid/stories/HideYAxis.stories.tsx
+++ packages/polaris-viz/src/components/Grid/stories/HideYAxis.stories.tsx
@@ -2,12 +2,13 @@ import type {Story} from '@storybook/react';
 
 export {META as default} from './meta';
 
-import {Grid, type GridProps} from '../Grid';
 import {CELL_GROUPS, DEFAULT_DATA, Template} from './data';
+import {GridProps} from '../Grid';
 
-export const Default: Story<GridProps> = Template.bind({});
+export const HideYAxis: Story<GridProps> = Template.bind({});
 
-Default.args = {
+HideYAxis.args = {
   data: DEFAULT_DATA,
   cellGroups: CELL_GROUPS,
+  yAxisOptions: {hide: true},
 };
