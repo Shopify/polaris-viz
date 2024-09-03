d33ce1a66d9a7d95c0d94cc83de359c17488079f
Jorge Moller <jmoller2106@gmail.com>
2024-09-03 18:04:32 -0300

    Add random data story

---
+++ packages/polaris-viz/src/components/Grid/stories/RandomData.stories.tsx
@@ -0,0 +1,13 @@
+import type {Story} from '@storybook/react';
+
+export {META as default} from './meta';
+
+import {RANDOM_CELL_GROUPS, RANDOM_DATA, Template} from './data';
+import {GridProps} from '../Grid';
+
+export const RandomData: Story<GridProps> = Template.bind({});
+
+RandomData.args = {
+  data: RANDOM_DATA,
+  cellGroups: RANDOM_CELL_GROUPS,
+};
