2625dd00143c3b9d6bd42cdf33fbd825ca2fa16b
Jorge Moller <jmoller2106@gmail.com>
2024-09-04 14:40:30 -0300

    Cleanup

--- packages/polaris-viz/src/components/Grid/stories/Default.stories.tsx
+++ packages/polaris-viz/src/components/Grid/stories/Default.stories.tsx
@@ -2,7 +2,7 @@ import type {Story} from '@storybook/react';
 
 export {META as default} from './meta';
 
-import {Grid, type GridProps} from '../Grid';
+import {type GridProps} from '../Grid';
 import {CELL_GROUPS, DEFAULT_DATA, Template} from './data';
 
 export const Default: Story<GridProps> = Template.bind({});
@@ -10,4 +10,10 @@ export const Default: Story<GridProps> = Template.bind({});
 Default.args = {
   data: DEFAULT_DATA,
   cellGroups: CELL_GROUPS,
+  xAxisOptions: {
+    label:  'Recency score'
+  },
+  yAxisOptions: {
+    label: 'Frequency + Monetary value score'
+  }
 };
