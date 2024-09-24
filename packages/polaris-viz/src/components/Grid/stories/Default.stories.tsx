1cf3bde0f36b85edc6df7ac65d104c5fbd012dc5
George <jmoller2106@gmail.com>
2024-09-24 14:08:03 -0300

    Add latest design changes

--- packages/polaris-viz/src/components/Grid/stories/Default.stories.tsx
+++ packages/polaris-viz/src/components/Grid/stories/Default.stories.tsx
@@ -3,12 +3,11 @@ import type {Story} from '@storybook/react';
 export {META as default} from './meta';
 
 import {type GridProps} from '../Grid';
-import {CELL_GROUPS, DEFAULT_DATA, Template} from './data';
+import {CELL_GROUPS, Template} from './data';
 
 export const Default: Story<GridProps> = Template.bind({});
 
 Default.args = {
-  data: DEFAULT_DATA,
   cellGroups: CELL_GROUPS,
   xAxisOptions: {
     label:  'Recency score'
