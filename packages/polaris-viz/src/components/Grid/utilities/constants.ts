650b69902a626cb906d96866d26433ddcec0bd20
George <jmoller2106@gmail.com>
2024-10-30 15:40:24 -0300

    Fix hardocded 120 value

---
+++ packages/polaris-viz/src/components/Grid/utilities/constants.ts
@@ -0,0 +1,12 @@
+export const TOOLTIP_WIDTH = 250;
+export const TOOLTIP_HEIGHT = 120;
+export const TOOLTIP_HORIZONTAL_OFFSET = 10;
+export const TOOLTIP_VERTICAL_OFFSET = 135;
+export const TOOLTIP_PADDING = 10;
+
+export const Y_LABEL_OFFSET = 20;
+export const Y_AXIS_LABEL_WIDTH = 50;
+export const X_AXIS_HEIGHT = 40;
+
+export const DEFAULT_GROUP_COLOR = '#B1C3F7';
+export const DEFAULT_TEXT_COLOR = '#FFFFFF';
