b528e42223f4c501cf75fcdda14fab3908a72713
Pablo Assanelli <pablo.assanelli@shopify.com>
2024-11-19 14:19:25 -0300

    fix, Tooltip needs to use createPortal to avoid cutting in edges

--- packages/polaris-viz/src/components/Grid/components/Tooltip.tsx
+++ packages/polaris-viz/src/components/Grid/components/Tooltip.tsx
@@ -1,6 +1,11 @@
+import {forwardRef} from 'react';
+import {createPortal} from 'react-dom';
 import type {CellGroup} from '../types';
 
 import styles from './Tooltip.scss';
+import {useRootContainer} from '../../../hooks/useRootContainer';
+
+const TOOLTIP_ID = 'polaris_viz_tooltip_root';
 
 interface TooltipProps {
   x: number;
@@ -8,47 +13,55 @@ interface TooltipProps {
   group: CellGroup | null;
 }
 
-export function Tooltip({x, y, group}: TooltipProps) {
-  if (!group) return null;
+export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+  function Tooltip({x, y, group}, ref) {
+    const container = useRootContainer(TOOLTIP_ID);
 
-  return (
-    <div
-      className={styles.TooltipContainer}
-      style={{
-        transform: `translate(${x}px, ${y}px)`,
-      }}
-      aria-label={group.name}
-    >
-      <div className={styles.Tooltip}>
-        <div className={styles.TooltipTitle}>{group.name}</div>
-        {group.metricInformation && (
-          <div className={styles.TooltipMetricInformation}>
-            {group.metricInformation}
+    return createPortal(
+      !group ? null : (
+        <div
+          ref={ref}
+          className={styles.TooltipContainer}
+          style={{
+            transform: `translate(${x}px, ${y}px)`,
+          }}
+          aria-label={group.name}
+        >
+          <div className={styles.Tooltip}>
+            <div className={styles.TooltipTitle}>{group.name}</div>
+            {group.metricInformation && (
+              <div className={styles.TooltipMetricInformation}>
+                {group.metricInformation}
+              </div>
+            )}
+            {group.description && (
+              <div className={styles.TooltipDescription}>
+                {group.description}
+              </div>
+            )}
+            {group.goal && (
+              <div className={styles.TooltipGoal}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2.5"
+                  stroke="currentColor"
+                  className={styles.TooltipIcon}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                  />
+                </svg>
+                <p className={styles.GroupGoal}>{group.goal}</p>
+              </div>
+            )}
           </div>
-        )}
-        {group.description && (
-          <div className={styles.TooltipDescription}>{group.description}</div>
-        )}
-        {group.goal && (
-          <div className={styles.TooltipGoal}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2.5"
-              stroke="currentColor"
-              className={styles.TooltipIcon}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-              />
-            </svg>
-            <p className={styles.GroupGoal}>{group.goal}</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
+        </div>
+      ),
+      container,
+    );
+  },
+);
