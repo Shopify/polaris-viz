e2dd189d13d5c0137468a7c23dfbc73e18e336c3
Carys Mills <carys.mills@shopify.com>
2024-11-29 21:46:11 -0500

    wip

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,12 +1,6 @@
-import {addons} from '@storybook/manager-api';
-import { themes } from '@storybook/theming';
+import {addons} from '@storybook/addons';
+import theme from './theme';
 
 addons.setConfig({
-  base: themes.dark,
-  brandTitle: 'Polaris-Viz',
-  brandUrl: 'https://github.com/Shopify/polaris-viz',
-  brandImage: logo,
-  fontBase:
-    "Inter, -apple-system, 'system-ui', 'San Francisco', 'Segoe UI', Roboto, 'Helvetica Neue', sans-serif",
-  fontCode: "Monaco, Consolas, 'Lucida Console', monospace"
+  theme,
 });
