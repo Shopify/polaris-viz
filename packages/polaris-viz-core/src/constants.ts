edff5c556f3c318fcb751cf26ba1a69d7a1d4753
Philipp S <philipp.schofer@shopify.com>
2025-01-20 12:25:02 -0500

    Slightly increase diagonal and vertical width for labels

--- packages/polaris-viz-core/src/constants.ts
+++ packages/polaris-viz-core/src/constants.ts
@@ -453,9 +453,9 @@ export const ELLIPSIS = '…';
 export const HORIZONTAL_LABEL_MIN_WIDTH = 46;
 export const HORIZONTAL_LABEL_TARGET_HEIGHT = 80;
 export const DIAGONAL_LABEL_MIN_WIDTH = 30;
-export const MAX_DIAGONAL_LABEL_WIDTH = 100;
-// Visible height of a 100px wide label at 45deg
-export const MAX_DIAGONAL_VISIBLE_HEIGHT = 80;
+export const MAX_DIAGONAL_LABEL_WIDTH = 120;
+// Visible height of a 120px wide label at 45deg
+export const MAX_DIAGONAL_VISIBLE_HEIGHT = 95;
 export const VERTICAL_LABEL_TARGET_WIDTH = 80;
 export const VERTICAL_LABEL_MIN_WIDTH = 14;
 export const DEFAULT_COMPONENTS: SvgComponents = {
