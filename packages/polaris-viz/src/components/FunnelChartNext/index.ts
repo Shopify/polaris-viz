0634cf061f0a3939d7866eabac19866f38b68b74
Michael Nesen <michael.nesen@shopify.com>
2025-02-13 18:14:26 +0000

    Label design changes

--- packages/polaris-viz/src/components/FunnelChartNext/index.ts
+++ packages/polaris-viz/src/components/FunnelChartNext/index.ts
@@ -1,3 +1,2 @@
 export {FunnelChartNext} from './FunnelChartNext';
 export type {FunnelChartNextProps} from './FunnelChartNext';
-export {getFunnelBarHeight} from './utilities/get-funnel-bar-height';
